44e99c7842c5418af0f48991619ca1c0a61c5358
Vitor Nascimento Penha <vvitornascimento@gmail.com>
2026-04-10 15:48:19 +0000

    Novato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,65 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    char estadoCarta1[50], estadoCarta2[50], codigoCarta1[10], codigoCarta2[10], nomeCidadeCarta1[50], nomeCidadeCarta2[50], cartaVencedora[50];
+    int  populacaoCarta1, populacaoCarta2, pontTurCarta1, pontTurCarta2;  
+    float areaCarta1, areaCarta2, pibCarta1, pibCarta2;
+
+    printf("--- CARTA 1 ---");
+    printf("\nEstado: ");
+    scanf(" %[^\n]", estadoCarta1);
+    getchar();
+
+    printf("\nCodigo: ");
+    scanf(" %[^\n]", codigoCarta1);
+
+    printf("\nCidade: ");
+    scanf(" %[^\n]", nomeCidadeCarta1);
+
+    printf("\nPopulação: ");
+    scanf("%d", &populacaoCarta1);
+
+    printf("\nPontos Turísticos: ");
+    scanf("%d", &pontTurCarta1);
+
+    printf("\nÁrea da Cidade: ");
+    scanf("%f", &areaCarta1);
+
+    printf("\nPIB: ");
+    scanf("%f", &pibCarta1);
+
+    printf("\n\n--- CARTA 2 ---");
+    printf("\nEstado: ");
+    scanf(" %[^\n]", estadoCarta2);
+    getchar();
+
+    printf("\nCodigo: ");
+    scanf(" %[^\n]", codigoCarta2);
+
+    printf("\nCidade: ");
+    scanf(" %[^\n]", nomeCidadeCarta2);
+
+    printf("\nPopulação: ");
+    scanf("%d", &populacaoCarta2);
+
+    printf("\nPontos Turísticos: ");
+    scanf("%d", &pontTurCarta2);
+
+    printf("\nÁrea da Cidade: ");
+    scanf("%f", &areaCarta2);
+
+    printf("\nPIB: ");
+    scanf("%f", &pibCarta2);
+
+    if(areaCarta1 > areaCarta2){
+        strcpy(cartaVencedora, estadoCarta1);
+    }
+    else{
+        strcpy(cartaVencedora, estadoCarta1);
+    }
+
+    printf("Por ter a maior área, a carta vencedora é: %s\n",cartaVencedora);
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
